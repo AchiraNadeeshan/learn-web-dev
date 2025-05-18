52e78695323b3753536187b868448a0a2d474d90
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-05-18 20:42:01 +0530

    feat: add animation for button click using js

--- 9. the drum kit/index.js
+++ 9. the drum kit/index.js
@@ -7,6 +7,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 
 }
@@ -15,6 +16,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keydown", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 // get the relevant sound
@@ -55,5 +57,15 @@ function makeSound(key) {
     }
 
 }
+
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")
+    }, 100);
 
 }
