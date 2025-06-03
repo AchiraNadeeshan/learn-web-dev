60cf24fb948bf4288d7d8cf1fdde14203666b499
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-06-03 06:55:56 +0530

    chore: remove server.js at incorrect path

--- 13. my express server/server.js
+++ 13. my express server/server.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+
+app.get('/about', function(req, res) {
+    res.send('About Page');
+});
+
+app.get('/contact', function(req, res) {
+    res.send('Contact Page');
+});
+
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+});
