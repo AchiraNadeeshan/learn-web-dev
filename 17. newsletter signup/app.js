47c9ab00b3aa4f930dac1d82df663deaaabb7a1a
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-06-08 04:30:06 +0530

    chore: add newsletter signup page using bootstrap and configured it

---
+++ 17. newsletter signup/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
