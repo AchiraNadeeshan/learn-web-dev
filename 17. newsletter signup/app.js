653a3e99c4f91ea9de679816a960c83386baff45
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-06-08 16:35:27 +0530

    feat: implement server communication with signup page

--- 17. newsletter signup/app.js
+++ 17. newsletter signup/app.js
@@ -1,19 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/signup.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+
+  var data = {};
+
+  const listId = "YOUR_LIST_ID";
+  const subscribingUser = {
+    firstName: "Prudence",
+    lastName: "McVankab",
+    email: "prudence.mcvankab@example.com",
+  };
+
+  async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+  }
+
+  run();
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
