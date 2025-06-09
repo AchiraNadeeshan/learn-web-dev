9019624239392c1d4a3a47eaeca455489b0bf786
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-06-09 21:20:26 +0530

    feat: add ejs and current day logic

--- 18. todolist v1/app.js
+++ 18. todolist v1/app.js
@@ -4,20 +4,41 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-    var today = new Date();
-
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("/weekend.html");
-    }else {
-        res.send("/weekday.html");
-    }
-
-
-
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      day = "Unknown Day";
+      console.log("Error: "+ currentDay);
+  }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(port, () => {
