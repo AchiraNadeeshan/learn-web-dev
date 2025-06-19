c56df9762a4a3678a593d6fd911d590132e69f94
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-06-19 17:42:13 +0530

    feat: add GET request handling

--- 21. wiki rest api/app.js
+++ 21. wiki rest api/app.js
@@ -3,19 +3,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-//TODO
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
 
-app.listen(3000, function() {
+app.get("/articles", async function (req, res) {
+  try {
+    const foundArticles = await Article.find();
+    res.send(foundArticles);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while fetching articles.");
+  }
+});
+
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
