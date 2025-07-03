5ffd2f6f4d3ce9a72fc81e7313c08e1fe4183632
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-07-03 08:59:06 +0530

    chore: add component for note showing

--- 23. keeper app/src/components/App.jsx
+++ 23. keeper app/src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello App</h1>
+      <Note />
       <Footer />
     </div>
   );
