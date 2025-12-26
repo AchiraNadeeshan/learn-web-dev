d4f35af9434e13dbab69695f36865081432fb424
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-12-27 02:02:29 +0530

    refactor: playsound and next sequence functions

--- 11. simon game/game.js
+++ 11. simon game/game.js
@@ -2,6 +2,15 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+});
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
@@ -11,6 +20,10 @@ function nextSequence() {
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
